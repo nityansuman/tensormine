e6e4ee8d2ca298a5723f50425656c84b18ffedde
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-23 22:33:48 +0530

    Updated Embedding Wrapper

--- tensorhub/text/blocks/embeddings.py
+++ tensorhub/text/blocks/embeddings.py
@@ -1,6 +1,6 @@
 """
 @Author: Kumar Nityan Suman
-@Date: 2019-06-20 14:22:05
+@Date: 2019-06-23 16:47:38
 """
 
 
@@ -9,92 +9,39 @@ import numpy as np
 from tensorflow import keras
 
 
-class Embeddings:
-    """Handle Embeddings."""
-    
-    def __init__(self):
-        pass
+class Embeddings(keras.layers.Layer):
+    """Custom embedding class implementation as a keras layer."""
 
-    @staticmethod
-    def create_vocabulary(corpus, type_embedding="word", num_words=10000):
-        """Create a sequence tokenizer and generate vocabulary. Supports both 'word' and 'char' sequences.
+    def __init__(self, input_dim, output_dim=300, max_seq_length=512, learn_embedding=True, embedding_matrix=None):
+        """Create an embedding layer for training an embedding or when loading pre-trained embeddings.
         
         Arguments:
-            corpus {list} -- A list of strings from which word-index or char-index mapping is created.
-            num_words {int} -- Maximum number of words to keep, based on word frequency. \
-                Only the most common (num_words-1) tokens will be kept. Not necessary when doing character embedding.
+            input_dim {int} -- Size of the vocab for the embeddings.
         
-        Returns:
-            TokenizerObject -- Tokenizer object to fit sequences.
-            Dict -- Vocabulary dictionary.
+        Keyword Arguments:
+            output_dim {int} -- Size of the embedding to be learn or pre-trained embedding. (default: {300})
+            max_seq_length {int} -- Maximum number of tokens allowed in a sample sequence. (default: {512})
+            learn_embedding {bool} -- Flag to set learning embedding as a part of the network. (default: {True})
+            embedding_matrix {numpy} -- If 'learn_embedding' is False, use this to load the pre-trained embeddings. (default: {None})        
         """
-        # Custom tokenizer
-        if type_embedding.lower() == "word":
-            # Word embeddings
-            tokenizer = keras.preprocessing.text.Tokenizer(num_words=num_words, oov_token="<UNK>")
-        else:
-            # Character embeddings
-            tokenizer = keras.preprocessing.text.Tokenizer(char_level=True, oov_token="<UNK>")
-        # Fit tokenizer on the corpus
-        tokenizer.fit_on_texts(corpus)
-        # Generate vocabulary
-        vocab = tokenizer.word_index
-        return tokenizer, vocab
-
-    @staticmethod
-    def load_embedding(filepath, token_index_mapping, embedding_dim=300):
-        """Create an embedding matrix from the given pre-trained vector.
-        
-        Arguments:
-            filepath {str} -- Path to load pre-trained embeddings (ex: glove).
-            embedding_dim {int} -- Dimension of the pre-trained embedding.
-            token_index_mapping {dict} -- A dictionary containing token-index mapping from the whole corpus.
-        
-        Returns:
-            Matrix -- A numpy matrix containing embeddings for each token in the token-index mapping.
-        """
-        # Placeholder for embedding
-        embedding_index = dict()
-        # Access file to load pre-trained embedding
-        with open(filepath, mode="r") as fp:
-            for line in fp:
-                values = line.split()
-                token = values[0:-dim]
-                coefs = values[-dim:]
-                embedding_index[token[0]] = coefs
-        # Create a weight matrix for token in training docs
-        embedding_matrix = np.zeros((len(token_index_mapping), embedding_dim))
-        # Create token-index mapping
-        for token, i in word_index.items():
-            embedding_vector = embeddings_index.get(token)
-            # Update embedding
-            if embedding_vector is not None:
-                embedding_matrix[i] = embedding_vector
-        return embedding_matrix
-    
-    @staticmethod
-    def EmbeddingLayer(vocab_size, embedding_dim, max_seq_length, learn_embedding, embedding_matrix):
-        """Create an embedding layer for training or to load pre-trained embeddings.
-        
-        Arguments:
-            vocab_size {int} -- [description]
-            embedding_dim {int} -- [description]
-            max_seq_length {int} -- [description]
-            learn_embedding {bool} -- [description]
-            embedding_matrix {numpyArray} -- [description]
-        
-        Returns:
-            layer -- Keras embedding layer to be used with othr model.
-        """
+        assert (learn_embedding == True and embedding_matrix == None) or (learn_embedding == False and embedding_matrix != None)
         if learn_embedding == True:
-            embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_seq_length, mask_zero=True)
-        else:
-            embedding_layer = keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, weights=[embedding_matrix], trainable=False, input_length=max_seq_length)
-        return embedding_layer
+            self.embedding_layer = keras.layers.Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_seq_length, mask_zero=True)
+        elif learn_embedding == False:
+            self.embedding_layer = keras.layers.Embedding(input_dim=input_dim, output_dim=output_dim, weights=[embedding_matrix], trainable=False, input_length=max_seq_length)
+
+    def call(self, x):
+        """Forward pass over the layer.
+        
+        Returns:
+            tensor -- Returns tensor output after embedding lookup.
+        """
+        x = self.embedding_layer(x)
+        return x
 
 
 class PositionEmbedding(keras.layers.Layer):
-    """Compute positional embedding."""
+    """Positional embedding layer implementation as a keras layer."""
     
     def __init__(self, mode="sum", size=None):
         """Class constructor.
@@ -111,10 +58,10 @@ class PositionEmbedding(keras.layers.Layer):
         """Forward pass for positional embeddings.
         
         Arguments:
-            x {Tensor} -- Input tensor to create position embedding.
+            x {tensor} -- Input tensor.
         
         Returns:
-            Tensor -- Created position embedding for the inputt tensor.
+            tensor -- Returns position embedding tensor.
         """
         # Update position embedding size
         if self.size == None or self.mode == "sum":
