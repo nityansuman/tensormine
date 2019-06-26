e443fb5914d6597937f673de12e38f94e15d9f4a
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-27 02:49:41 +0530

    Renamed

--- examples/run_text_classifiers.py
+++ examples/run_text_classifiers.py
@@ -1,8 +1,27 @@
 """
 @Author: Kumar Nityan Suman
 @Date: 2019-05-28 02:56:59
-"""
 
+Copyright (c) 2018 The Python Packaging Authority
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 # Load packages
 import numpy as np
@@ -10,10 +29,8 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
-
-# Custom imports
-from text.blocks.embeddings import Embeddings
-from text.cooked_models.classifier import SimpleTextClassification
+from tensorhub.text.wrapper.embeddings import Embeddings
+from tensorhub.text.cooked_models.classification import SimpleTextClassification
 
 # Multiclass Text Classification on 'News Healines' Dataset
 
