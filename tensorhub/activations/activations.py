26278658c5181c2d49e1225b621c540371b2e30b
nityansuman <nityan.suman@gmail.com>
2020-09-18 12:50:38 +0530

    Updated activation module

--- tensorhub/activations/activations.py
+++ tensorhub/activations/activations.py
@@ -187,14 +187,3 @@ def selu(x):
 	alpha = 1.6732632423543772848170429916717
 	scale = 1.0507009873554804934193349852946
 	return scale * elu(x, alpha)
-
-def mish(x):
-	"""[summary]
-
-	Arguments:
-		x {[type]} -- [description]
-
-	Returns:
-		[type] -- [description]
-	"""
-	return x * tanh(softplus(x))
