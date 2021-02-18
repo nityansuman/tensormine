b840c3f973465f995efebcea60fea95f106f7d88
nityansuman <nityan.suman@gmail.com>
2021-02-18 22:49:45 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorHub Authors. All Rights Reserved.
+# Copyright 2021 The TensorHub Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ setuptools.setup(
     version="1.0.0",
     author="Kumar Nityan Suman",
     author_email="nityan.suman@gmail.com",
-    description="TensorHub is a library of deep learning models and lego like interlocking blocks designed to make deep learning more accessible and accelerate ML research using TensorFlow 2.0",
+    description="TensorHub is a library built on top of TensorFlow 2 to provide simple, modular and repeatable abstractions to accelerate deep learning research.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nityansuman/tensorhub",
@@ -40,14 +40,10 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
